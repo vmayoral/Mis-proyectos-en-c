5514247f47201a3c819bd16ac9e8b3af4145e88a
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2015-12-10 08:24:00 +0100

    Propuesta de implementación sencilla

--- numero_o_palabra.c
+++ numero_o_palabra.c
@@ -6,29 +6,32 @@ int main()
 	char cadena[100];
 	char *p;
 	int espacios= 0, letras = 0, numeros= 0;
-
-	p = cadena;
+	int longitud_cadena, i;
 
 	printf("Introduzca numero/palabra:%s", cadena);
-	scanf("%s", &cadena);
 
+	gets(cadena);
+	longitud_cadena = strlen(cadena);
 
-
-	while ( *p != "\0")
-	{
-		if ( *p == " ") espacios++;
-		if ( *p == "abcdefghijklmnñopqrstuvwyzABCDEFGHIJKLMNÑOPKRSTUVWYZ") letras++;
-		if (*p == "0123456789") numeros++;
-		p++;
+	for (i=0; i<longitud_cadena; i++){
+		// Funciones sacadas de poner "man 3 isdigit" en la línea de comandos
+		printf("caracter %c\n", cadena[i]);
+		if (isdigit(cadena[i])){
+			printf("número\n");
+			numeros++;
+		} else if (isspace(cadena[i])){
+			printf("espacio\n");
+			espacios++;
+		} else {
+			printf("letra\n");
+			letras++;
+		}
 	}
 
-	printf ("En la cadena \"%s\" hay:\n", cadena);
-	printf ("%i espacios\n", espacios);
-	printf ("%i letras\n", letras);
-	printf ("%i numeros\n", numeros);
-	printf ("La largura de la cadena es:\n" (strlen(cadena));
-
-
-
+	printf("La cadena introducida es %s\n", cadena);
+	printf("  Número de espacios: %d\n", letras);
+	printf("  Número de letras: %d\n", letras);
+	printf("  Número de dígitos: %d\n", numeros);
+	printf("La longitud total de la cadena es %d\n", longitud_cadena);
 	return 0;
 }
